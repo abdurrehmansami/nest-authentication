db8c021119308900a044b623f6d5140862d24a31
abdurrehmansami <abdurrehmansherazi190@gmail.com>
2024-10-22 13:06:15 +0500

    added exceotion filter

--- src/exception/http-exception.filter.ts
+++ src/exception/http-exception.filter.ts
@@ -4,6 +4,7 @@ import {
   ArgumentsHost,
   HttpException,
   HttpStatus,
+  BadRequestException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
@@ -13,18 +14,47 @@ export class AllExceptionsFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+    // const status =
+    //   exception instanceof HttpException
+    //     ? exception.getStatus()
+    //     : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const message = exception.message || 'Internal server error';
+    // const message = exception.message || 'Internal server error';
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      message: message,
-    });
+    // response.status(status).json({
+    //   statusCode: status,
+    //   timestamp: new Date().toISOString(),
+    //   path: request.url,
+    //   message: message,
+    // });
+    if (exception instanceof BadRequestException) {
+      const status = exception.getStatus();
+      const exceptionResponse: any = exception.getResponse();
+
+      // Here, we handle the validation error response with more details
+      response.status(status).json({
+        statusCode: status,
+        message: exceptionResponse.message || 'Bad Request',
+        error: exceptionResponse.error || 'Validation error',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    } else if (exception instanceof HttpException) {
+      const status = exception.getStatus();
+      response.status(status).json({
+        statusCode: status,
+        message: exception.message,
+        error: exception.name,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    } else {
+      response.status(500).json({
+        statusCode: 500,
+        message: 'Internal server error',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    }
   }
 }
