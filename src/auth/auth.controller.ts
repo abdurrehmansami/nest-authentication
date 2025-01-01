8ea55a5d5743fabcf69aeed0c043be2d1d997236
Abdul Rehman <abdurrehmansherazi190@gmail.com>
2025-01-01 16:01:29 +0500

    com2 on develop

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -64,7 +64,7 @@ export class AuthController {
   }
   @Get('getHello')
   async getHello(@Headers() headers) {
-    console.log('HEADERS', headers);
+    // console.log('HEADERS', headers);
 
     return this.appService.getHello();
   }
